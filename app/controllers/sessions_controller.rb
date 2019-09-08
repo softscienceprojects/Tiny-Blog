141fc81d48c60cd070ff083bb30194c1a58b1588
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-08 16:16:36 +0100

    Log-out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,20 @@
 class SessionsController < ApplicationController
 
     def new
-
+        
     end
 
     def create
-
+        @user = User.find_by(username: params[:user][:username]).try(:authenticate, params[:user][:password])
+        return redirect_to new_session_path unless !!@user
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
     end
 
     def destroy
-
+        session.delete(:user_id) 
+        redirect_to new_session_path
     end
 
-    
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+before_action :find_current_user, only: [:show, :edit, :update, :destroy]
+before_action :require_login
+skip_before_action :require_login, only: [:new, :create]
+
+    def show
+
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.create(user_params)
+        return redirect_to new_user_path unless @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+        
+    end
+
+    private
+
+    def find_current_user
+        @user = User.find(params[:id])
+    end
+
+    def require_login
+        redirect_to new_session_path unless session.include? :user_id
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :password, :password_confirmation)
+    end
 end
