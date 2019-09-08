4c66c2f10afd1fe1db2ea8466eae23524132387f
softscienceprojects <erin.nicole.johnson@gmail.com>
2019-09-08 17:24:37 +0100

    new, edit, delete post, plus require_login for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,23 @@
 class PostsController < ApplicationController
     before_action :find_post, only: [:show, :edit, :update, :destroy]
+    before_action :require_login
+    skip_before_action :require_login, only: [:index, :show]
 
     def index
 
     end
 
     def show
-
+        
     end
 
     def new
-
+        @post = Post.new
     end
 
     def create
-
+        @post = Post.create(post_params)
+        redirect_to post_path(@post)
     end
 
     def edit
@@ -36,6 +39,10 @@ class PostsController < ApplicationController
     end
 
     def post_params
-        params.require(:post).permit(:title, :content)
+        params.require(:post).permit(:title, :content, :user_id)
+    end
+
+    def require_login
+        redirect_to new_session_path unless session.include? :user_id
     end
 end
